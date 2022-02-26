7513414f59ec5634886620f07549f8774d78eac4
First Last <email>
2022-02-25 17:32:40 -0700

    Updated Main.java with in terminal command responses.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,53 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Calculator calculator = new Calculator();
+        Scanner sc = new Scanner(System.in);
 
-        String testRun = args[0];
-        int val1 = Integer.parseInt(args[1]);
-        int val2=0;
-        if (args.length > 2) {
-            val2 = Integer.parseInt(args[2]);
+        System.out.println("Welcome to the calculator\nEnter a command: ");
+        String line = sc.nextLine();
+
+        for (int i = 0; i < 1;) {
+            Scanner values = new Scanner(line);
+            String testRun = values.next();
+            int val1 = Integer.parseInt(values.next());
+            int val2 = 0;
+
+            if (values.hasNext()) {
+                val2 = Integer.parseInt(values.next());
+            }
+
+
+            int result = 0;
+            switch (testRun) {
+                case "exit":
+                    i += 1;
+                    break;
+                case "add":
+                    result = calculator.add(val1, val2);
+                    break;
+                case "subtract":
+                    result = calculator.subtract(val1, val2);
+                    break;
+                case "multiply":
+                    result = calculator.multiply(val1, val2);
+                    break;
+                case "divide":
+                    result = calculator.divide(val1, val2);
+                    break;
+                case "fibonacci":
+                    result = calculator.fibonacciNumberFinder(val1);
+                    break;
+                case "binary":
+                    result = Integer.parseInt(calculator.intToBinaryNumber(val1));
+                    break;
+            }
+
+            System.out.println(result);
+            line = sc.nextLine();
         }
-
-        int result = 0;
-        switch (testRun) {
-            case "add":
-                result = calculator.add(val1, val2);
-                break;
-            case "subtract":
-                result = calculator.subtract(val1, val2);
-                break;
-            case "multiply":
-                result = calculator.multiply(val1, val2);
-                break;
-            case "divide":
-                result = calculator.divide(val1, val2);
-                break;
-            case "fibonacci":
-                result = calculator.fibonacciNumberFinder(val1);
-                break;
-            case "binary":
-                result = Integer.parseInt(calculator.intToBinaryNumber(val1));
-                break;
-        }
-
-        System.out.println(result);
     }
 }
